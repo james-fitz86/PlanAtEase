9a00d08c3e2a29c8d6929aea6fd32f4f252e6c14
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-08-24 15:32:20 +0100

    added cors to middleware, moved CORS_ALLOWED_ORIGINS to .env

--- planatease-backend/config/settings.py
+++ planatease-backend/config/settings.py
@@ -36,8 +36,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "rest_framework",
     "corsheaders",
+    "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,6 +149,4 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+CORS_ALLOWED_ORIGINS = [os.getenv("CORS_ALLOWED_ORIGINS")]
