d0c9e6c1e21b1d76f7f7fb5389273ac21cfa34f1
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-09-13 14:09:47 +0100

    added urls for tripitems

--- planatease-backend/trips/urls.py
+++ planatease-backend/trips/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     TripListCreateView, TripRetrieveUpdateDestroyView,
     TripMemberListCreateView, TripMemberDestroyView,
+    TripItemListCreateView, TripItemRetrieveUpdateDestroyView,
 )
 
 app_name = "trips"
@@ -11,4 +12,6 @@ urlpatterns = [
     path("<int:pk>/", TripRetrieveUpdateDestroyView.as_view(), name="trip-detail"),
     path("<int:trip_id>/members/", TripMemberListCreateView.as_view(), name="tripmember-list-create"),
     path("<int:trip_id>/members/<int:user_id>/", TripMemberDestroyView.as_view(), name="tripmember-destroy"),
+    path("<int:trip_id>/items/", TripItemListCreateView.as_view(), name="tripitem-list-create"),
+    path("<int:trip_id>/items/<int:item_id>/", TripItemRetrieveUpdateDestroyView.as_view(), name="tripitem-detail"),
 ]
