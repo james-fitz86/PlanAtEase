d3c5ae2b5f4c062a9ea521c4fcdbef557f319004
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-10-01 10:10:06 +0100

    dded role update routes by wiring member detail endpoints to TripMemberDetailView

--- planatease-backend/trips/urls.py
+++ planatease-backend/trips/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from .views import (
     TripListCreateView, TripRetrieveUpdateDestroyView,
-    TripMemberListCreateView, TripMemberDestroyView,
+    TripMemberListCreateView, TripMemberDetailView,
     TripItemListCreateView, TripItemRetrieveUpdateDestroyView,
 )
 
@@ -12,19 +12,19 @@ urlpatterns = [
 
     path("<uuid:uid>/", TripRetrieveUpdateDestroyView.as_view(), name="trip-detail-by-uid"),
     path("<uuid:uid>/members/", TripMemberListCreateView.as_view(), name="tripmember-list-create-by-uid"),
-    path("<uuid:uid>/members/<int:user_id>/", TripMemberDestroyView.as_view(), name="tripmember-destroy-by-uid"),
+    path("<uuid:uid>/members/<int:user_id>/", TripMemberDetailView.as_view(), name="tripmember-detail-by-uid"),
     path("<uuid:uid>/items/", TripItemListCreateView.as_view(), name="tripitem-list-create-by-uid"),
     path("<uuid:uid>/items/<int:item_id>/", TripItemRetrieveUpdateDestroyView.as_view(), name="tripitem-detail-by-uid"),
 
     path("<int:pk>/", TripRetrieveUpdateDestroyView.as_view(), name="trip-detail"),
     path("<int:trip_id>/members/", TripMemberListCreateView.as_view(), name="tripmember-list-create"),
-    path("<int:trip_id>/members/<int:user_id>/", TripMemberDestroyView.as_view(), name="tripmember-destroy"),
+    path("<int:trip_id>/members/<int:user_id>/", TripMemberDetailView.as_view(), name="tripmember-detail"),
     path("<int:trip_id>/items/", TripItemListCreateView.as_view(), name="tripitem-list-create"),
     path("<int:trip_id>/items/<int:item_id>/", TripItemRetrieveUpdateDestroyView.as_view(), name="tripitem-detail"),
 
     re_path(r'^(?P<slug>[A-Za-z][-\w]*)/$', TripRetrieveUpdateDestroyView.as_view(), name="trip-detail-by-slug"),
     re_path(r'^(?P<slug>[A-Za-z][-\w]*)/members/$', TripMemberListCreateView.as_view(), name="tripmember-list-create-by-slug"),
-    re_path(r'^(?P<slug>[A-Za-z][-\w]*)/members/(?P<user_id>\d+)/$', TripMemberDestroyView.as_view(), name="tripmember-destroy-by-slug"),
+    re_path(r'^(?P<slug>[A-Za-z][-\w]*)/members/(?P<user_id>\d+)/$', TripMemberDetailView.as_view(), name="tripmember-detail-by-slug"),
     re_path(r'^(?P<slug>[A-Za-z][-\w]*)/items/$', TripItemListCreateView.as_view(), name="tripitem-list-create-by-slug"),
     re_path(r'^(?P<slug>[A-Za-z][-\w]*)/items/(?P<item_id>\d+)/$', TripItemRetrieveUpdateDestroyView.as_view(), name="tripitem-detail-by-slug"),
 ]
